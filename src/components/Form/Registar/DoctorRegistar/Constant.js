a1712ef42c9eb7151efdc85db50a9126b827d609
Himanshu Yadav <himanshu@letsmeet.tvranedu.com>
2022-11-15 14:58:14 +0530

    front page

--- src/components/Form/Registar/DoctorRegistar/Constant.js
+++
@@ -1,6 +0,0 @@
-export const INITIAL_DATA = {
-    doctorName: '',
-    speciality: '',
-    email:'',
-    password:'',
-  };
--- src/components/Form/Registar/PatientRegistar/constant.js
+++
@@ -1,5 +0,0 @@
-export const INITIAL_DATA = {
-    patientName: '',
-    email:'',
-    password:'',
-  };
