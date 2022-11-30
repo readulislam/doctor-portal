1559c4b27be5cb13229f7c6b17322b52dc4b7740
Himanshu Yadav <himanshu@letsmeet.tvranedu.com>
2022-11-30 19:52:12 +0530

    event

--- src/components/Modal/EventModal.js
+++ src/components/Modal/EventModal.js
@@ -1,19 +1,50 @@
 import { Button, Modal } from 'flowbite-react';
-import React from 'react'
+import React, { useState } from 'react'
+import ReactDatePicker from 'react-datepicker';
 
 const AvailabilityModal = ({setOpen,open,setEvent,setEventTitle,eventTitle,event,eventDate}) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [input, setInput] = useState([
+    {
+      inputTag:<input type='text' onChange={(e)=>{setEventTitle(e.target.value)}}/>
+    },
+  ]);
+  const [endDate, setEndDate] = useState(null);
+  const handleAdd=()=>{
+    setInput(input=>{return[...input,{inputTag:<input type='text' onChange={(e)=>{setEventTitle(e.target.value)}}/>}]})
+  }
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+  const handleSubmit=()=>{
+    setEvent(event=>{return[...event,{title:eventTitle,date:eventDate}]});
+    setOpen(false);
+    console.log(event);
+  }
   return (
     <React.Fragment>
-      <Modal show={open} position="center" onClose={() => setOpen(false)}>
+      <Modal show={open} position="center" onClose={() => {setOpen(false)}}>
         <form>
           <Modal.Header>Enter Time</Modal.Header>
           <Modal.Body>
             <div className="space-y-6 p-6">
-                <input type='text' onChange={(e)=>{setEventTitle(e.target.value)}}/>
+            <ReactDatePicker
+              selected={startDate}
+              onChange={onChange}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              inline
+            />
+                {input.map((value)=>(
+                  <div>
+                    {value.inputTag}
+                  </div>
+                ))}
+                <Button onClick={handleAdd} >Add</Button>
             </div>
-              
-
-              
           </Modal.Body>
           <Modal.Footer className="flex justify-between">
             <Button color="gray" onClick={() => setOpen(false)}>
@@ -22,7 +53,7 @@ const AvailabilityModal = ({setOpen,open,setEvent,setEventTitle,eventTitle,event
             <Button
               className="px-2 "
               gradientDuoTone="cyanToBlue"
-              onClick={()=>{setEvent(event=>{return[...event,{title:eventTitle,date:eventDate}]});setOpen(false);console.log(event);}}
+              onClick={handleSubmit}
             >
               Submit
             </Button>
