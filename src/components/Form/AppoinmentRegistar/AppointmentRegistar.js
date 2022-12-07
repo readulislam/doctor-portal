861ee50518583e6d71273be9a75e0ae67097dd99
readul-islam <developer.readul@gmail.com>
2022-12-08 00:21:50 +0600

    update

--- src/components/Form/AppoinmentRegistar/AppointmentRegistar.js
+++ src/components/Form/AppoinmentRegistar/AppointmentRegistar.js
@@ -1,18 +1,19 @@
-import { TextInput } from 'flowbite-react';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import AppointmentBookedModal from '../../Modal/AppointmentBookedModal';
-import ModalView from '../../Modal/ModalView'
+import ModalView from '../../Modal/ModalView';
 import RegistarModal from '../../Modal/RegistarModal';
 
-const AppointmentRegistar = ({ open, setOpen ,name,location,speciality}) => {
+const AppointmentRegistar = ({ open, setOpen ,name,location,speciality,doctorId}) => {
     const [existingUser, setExistingUser] = useState(false);
     const [time, setTime] = useState("");
     const [date, setDate] = useState("");
     const [registarOpen, setRegistarOpen] = useState(false);
     const [done, setDone] = useState(false);
+  
+   
   return (
     <div> 
-        <ModalView open={open} setDate={setDate}  setTime={setTime} setOpen={setOpen} name={name} speciality={speciality} setRegistarOpen={setRegistarOpen}  existingUser={existingUser} location={location} />
+        <ModalView open={open} setDate={setDate} doctorId={doctorId} setTime={setTime} setOpen={setOpen} name={name} speciality={speciality} setRegistarOpen={setRegistarOpen}  existingUser={existingUser} location={location} />
         {(!existingUser && registarOpen) &&<RegistarModal open={registarOpen} setOpen={setRegistarOpen} location={location} setExistingUser={setExistingUser} setDone={setDone}/>}
         {done && <AppointmentBookedModal time={time} date={date} done={done} setDone={setDone} name={name} />}
 
