e637d852339ba94c9f2f85cf822ae0b16f8ad1d5
readul-islam <developer.readul@gmail.com>
2022-12-08 18:51:25 +0600

    o

--- src/APi/api.js
+++ src/APi/api.js
@@ -22,39 +22,81 @@ ADD_EVENT_TYPE:() =>'/add-eventType',
 const URL =(End_Point) => `${BaseUrl}${End_Point}`
 
 
-//get all Departments
+/*------------------------
+get all departments 
+ */
 export const ListDepartments = async()=>{
     const {data} = await axios.get(URL(END_POINTS.GET_DEPARTMENTS()))
     return  data
 }
   
 
-//get all Departments
+/*------------------------
+get all hospitals 
+ */
 export const ListHospitals = async()=>{
     const {data} = await axios.get(URL(END_POINTS.GET_HOSPITALS()))
     return  data
 }
-  
-export const addDoctor = async() =>{
 
+
+/* it takes some info as like below
+
+"name":"DR. INDARJEET GUPTA",
+"eductaion":"PHD",
+"departmentId":9,
+"hospitalId":5,
+"contactNo":"+969828044375",
+"designation":"Nephrology",
+"img":"https://api.parashospitals.com/storage/doctors/September2022/0gt3x16BfPMzFkP5UmDT.jpg    " 
+*/
+export const AddDoctor = async(doctorInfo) =>{
+    const {data} = await axios.get(URL(END_POINTS.ADD_DOCTOR()),{
+        ...doctorInfo
+    })
+    return  data
 }
-export const getDoctors = async() =>{
+export const ListDoctors = async() =>{
     const {data} = await axios.get(URL(END_POINTS.GET_DOCTORS()))
     return  data
 }
-export const patientRegister = async(patientInfo) =>{
+
+/*-------------------------
+ it takes user or patient info as like below
+ "title": "Mr",
+"firstName": "himanshu",
+"middleName": "readul",
+"lastName": "islam",
+"contact": "9876543210",
+"dateOfBirth": "13/2/2000",
+"address": "3216 ug cyii",
+"location": "msmart,jaipur",
+"gender": "male",
+"country": "india",
+"state": "delhi",
+"stateId":9,
+"cityId":1,
+"city": "delhi",
+"pinCode": "555555",
+"martialStatus": "single"
+ */
+export const PatientRegister = async(patientInfo) =>{
     const {data} = await axios.get(URL(END_POINTS.ADD_PATIENT()),{
         ...patientInfo
     })
     return  data
 }
-/* it takes some info as like below
+
+
+/*------------------------ 
+it takes some info as like below
+
 "date":"17/12/2022",
 "doctorId":2,
 "overall_Availability":"",
 "current_Availability":""
 */
-export const addDateOverride = async(info) =>{
+export const AddDateOverride = async(info) =>{
     const {data} = await axios.get(URL(END_POINTS.ADD_DATE_OVERRIDE()),{
        ...info
     })
@@ -63,7 +105,9 @@ export const addDateOverride = async(info) =>{
 
 
 
-/* it takes more info as like below
+/*------------------------ 
+it takes more info as like below
+
 "doctorId":16,
 "eventTypeId":8,
 "monday":"9.00-17.00",
@@ -77,19 +121,18 @@ export const addDateOverride = async(info) =>{
 "endDate":"6/12/2023"
 Note: time format 24 hours as like 13:00 or 17:00
 */
-export const addWeeklyAvailability = async(availabilityINfo) =>{
+export const AddWeeklyAvailability = async(availabilityINfo) =>{
     const {data} = await axios.get(URL(END_POINTS.ADD_PATIENT()),{
         ...availabilityINfo
     })
     return  data
 }
-export const getWeeklyAvailability = async() =>{
 
-}
 
-/*  get slots by doctor availability ;
+/*  ------------------------
+ get slots by doctor availability ;
  it takes two query {date(data-format as like 8/12/2022)  and doctorId} */
-export const slotsByDoctorAvailability = async(query) =>{
+export const SlotsByDoctorAvailability = async(query) =>{
     const {data} = await axios.get(URL(END_POINTS.GET_SLOTS()),{
         query:{
             ...query
@@ -99,7 +142,9 @@ export const slotsByDoctorAvailability = async(query) =>{
 }
 
 
-// get all states 
+/*-------------------------
+get all states
+ */ 
 export const ListStates = async()=>{
     const {data} = await axios.get(URL(END_POINTS.GET_STATES()))
     return  data
